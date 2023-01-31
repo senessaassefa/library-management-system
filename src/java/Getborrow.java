4d14614096e8e8fbde50326af497cccf26af8174
senessaassefa <senessa.a88@gmail.com>
2023-01-30 19:50:53 -0800

    commited

---
+++ src/java/Getborrow.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author margaa
+ */
+public class Getborrow {
+    private String bookname;
+     private String studentname;
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public String getStudentname() {
+        return studentname;
+    }
+
+    public String getBookid() {
+        return bookid;
+    }
+
+    public String getStudentid() {
+        return studentid;
+    }
+      private String bookid;
+       private String studentid;
+       
+}
